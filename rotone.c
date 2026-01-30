fec2bce63a05c1392304812fb9ee7f8fe3ce6c64
Philippe <philippekaway@gmail.com>
2026-01-29 21:17:14 -0300

    refactor: clean up formatting and improve readability in rotone.c

--- rotone.c
+++ rotone.c
@@ -2,31 +2,27 @@
 
 int main(int argc, char **argv)
 {
-    int i;
+    int  i;
     char letter;
 
     i = 0;
-    if(argc == 2)
+    if (argc == 2)
     {
-        while(argv[1][i])
+        while (argv[1][i])
         {
             letter = argv[1][i];
-            if(argv[1][i] >= 'A' && argv[1][i] <= 'Y')
-            {
+            if (letter >= 'a' && letter <= 'y')
                 letter += 1;
-            }
-            if(argv[1][i] >= 'a' && argv[1][i] <= 'y')
-            {
-                letter +=1;
-            }
-            if(argv[1][i] >= 'z' && argv[1][i] <= 'Z')
-            {
-                letter -=25;
-            }
+            else if (letter >= 'A' && letter <= 'Y')
+                letter += 1;
+            else if (letter == 'z')
+                letter = 'a';
+            else if (letter == 'Z')
+                letter = 'A';
             write(1, &letter, 1);
             i++;
         }
     }
     write(1, "\n", 1);
-    return(0);
+    return (0);
 }
